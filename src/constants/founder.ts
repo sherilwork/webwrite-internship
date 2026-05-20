fb9bbdbad5f38757b31856f8b308f6ff8cf714a2
Sheril <sherilworks.in@gmail.com>
2026-05-20 14:27:04 +0000

    /home/user/studio/public/team/team-memeber-1.png change image baki sab s

--- src/constants/founder.ts
+++ src/constants/founder.ts
@@ -4,7 +4,7 @@ export const founders = [
     id: 1,
     name: "Rishi Tiwari",
     role: "Founder & CEO",
-    image: "/team/team-1.png",
+    image: "/team/team-memeber-1.png",
     fallbackImage: "https://picsum.photos/seed/founder/800/1000",
     quote:
       "Founder & CEO at WebWrite Services | Full Stack Web Developer | Entrepreneur with a Business Mindset Personality.",
